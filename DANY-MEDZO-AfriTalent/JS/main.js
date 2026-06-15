13913b8e2968f67028d3ed5bcf666a2cc1687bec
Dany MEDZO <maidendany1@gmail.com>
2026-06-15 17:14:51 +0000

    JS : filtrage dynamique des freelances et validation du formulaire de contact

--- DANY-MEDZO-AfriTalent/JS/main.js
+++ DANY-MEDZO-AfriTalent/JS/main.js
@@ -68,3 +68,122 @@ if(form){
     });
 
 }
+
+/* ==========================
+   FILTRAGE DES FREELANCES
+========================== */
+
+const categorie = document.getElementById("categorie");
+
+if(categorie){
+
+    categorie.addEventListener("change", () => {
+
+        const valeur = categorie.value;
+
+        const cartes =
+            document.querySelectorAll(".freelance-card");
+
+        cartes.forEach(carte => {
+
+            const cat =
+                carte.getAttribute("data-category");
+
+            if(valeur === "all" || cat === valeur){
+
+                carte.style.display = "block";
+
+            }else{
+
+                carte.style.display = "none";
+
+            }
+
+        });
+
+    });
+
+}
+
+/* ==========================
+   VALIDATION FORMULAIRE
+========================== */
+
+const form =
+document.getElementById("contactForm");
+
+if(form){
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const nom =
+    document.getElementById("nom").value.trim();
+
+    const prenom =
+    document.getElementById("prenom").value.trim();
+
+    const email =
+    document.getElementById("email").value.trim();
+
+    const message =
+    document.getElementById("message").value.trim();
+
+    document.getElementById("nomError").textContent = "";
+    document.getElementById("prenomError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("messageError").textContent = "";
+    document.getElementById("successMessage").textContent = "";
+
+    let valide = true;
+
+    if(nom === ""){
+
+        document.getElementById("nomError")
+        .textContent = "Le nom est obligatoire";
+
+        valide = false;
+    }
+
+    if(prenom === ""){
+
+        document.getElementById("prenomError")
+        .textContent = "Le prénom est obligatoire";
+
+        valide = false;
+    }
+
+    const regex =
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!regex.test(email)){
+
+        document.getElementById("emailError")
+        .textContent = "Email invalide";
+
+        valide = false;
+    }
+
+    if(message.length < 20){
+
+        document.getElementById("messageError")
+        .textContent =
+        "Le message doit contenir au moins 20 caractères";
+
+        valide = false;
+    }
+
+    if(valide){
+
+        document.getElementById("successMessage")
+        .textContent =
+        "Votre message a été envoyé avec succès !";
+
+        form.reset();
+
+    }
+
+});
+
+}
